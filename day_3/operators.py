7c39314f103a543fc6540f6ed26d661f2f3fc883
Lakshan Attanayake <lakshan.attanayake95@gmail.com>
2025-10-02 14:12:36 +0530

    operators day

---
+++ day_3/operators.py
@@ -0,0 +1,27 @@
+age = 30
+height = 5.9
+com_numbre = 70j
+
+triangle_base = input("Enter triangle base: ")
+triangle_height = input("Enter triangle height: ")
+
+area_of_triangle = 0.5 * float(triangle_base) * float(triangle_height)
+print("Area of triangle:", area_of_triangle)
+
+side_a = input("Enter side a: ")
+side_b = input("Enter side b: ")    
+side_c = input("Enter side c: ")
+
+perimitor  = float(side_a) + float(side_b) +float(side_c)
+print("Perimitor of triangle:", perimitor)
+
+rec_length = input("Enter reqular length: ")
+rec_width = input("Enter reqular width: ")
+area_of_rectangle = float(rec_length) * float(rec_width)
+print("Area of rectangle:", area_of_rectangle)
+
+
+
+
+for i in range(1, 6):
+        print(f"{i} 1 {i} {i**2} {i**3}")
