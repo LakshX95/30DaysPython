aaa4fcce2a334b48b7d154b0495a5312ee649e79
Lakshan Attanayake <lakshan.attanayake95@gmail.com>
2025-10-02 11:42:59 +0530

    variables day

---
+++ day_2/variables.py
@@ -0,0 +1,35 @@
+#Day 2: 30 Days of python programming
+first_name = 'Lakshan'
+last_name = 'Attanayake'
+full_name = first_name, last_name   
+country = 'Sri Lanka'
+city = 'Colombo'
+age = 30
+is_married = False
+is_light_on = True
+skills = ['HTML', 'CSS', 'JavaScript', 'Python']
+
+print(type(first_name))
+print(len(first_name))
+num_one = 5
+num_two = 4
+total = num_one + num_two
+print(total)
+diff = num_one - num_two
+print(diff) 
+product = num_one * num_two
+print(product)
+division = num_one / num_two
+print(division)
+remainder = num_one % num_two
+print(remainder)
+floor_division = num_one // num_two
+print(floor_division)
+
+area_of_circle = 3.14 * 30**2
+print(area_of_circle)
+circum_of_circle = 2 * 3.14 * 30
+print(circum_of_circle)
+radius = input("Enter radius: ")
+area = 3.14 * float(radius)**2
+print("Area of circle:", area)  
