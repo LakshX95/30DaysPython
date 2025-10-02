3e6915489c2df9f5fcd8fac453839a176f801af3
Lakshan Attanayake <lakshan.attanayake95@gmail.com>
2025-10-02 11:43:55 +0530

    updated

---
+++ day_1/day_1.py
@@ -0,0 +1,41 @@
+x = 3
+y = 4
+
+print("addition:", x + y)
+print("subtraction:", x - y)
+print("multiplication:", x * y)
+print("modulus:", x % y)
+print("division:", x / y)
+print("exponential:", x ** y)
+print("floor division:", x // y)
+
+name = "Lakshan"
+surname = "Attanayake"
+country = "Sri Lanka"
+quote = "Im enjoying 30 days of python challange"
+
+print("Hello, my name is", name, surname, ". I live in", country + ". and", quote)
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Lakshan'))
+
+#Euclidian distance
+q1 = 3
+p1 = 2
+
+q2 = 8
+p2 = 10
+distance = ((q1 - p1)**2 + (q2 - p2)**2)**0.5
+print("Euclidian distance:", distance)
+
+num = '10.6'
+num = float(num)
+print(num)
+print(int(num))
+
+name = 'Lakshan'
+print(list(name))
