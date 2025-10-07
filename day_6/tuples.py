159fa676a8b11a9ba9cf2102d8593e5ffbe9154f
Lakshan Attanayake <lakshan.attanayake95@gmail.com>
2025-10-07 15:56:00 +0530

    tuples day

---
+++ day_6/tuples.py
@@ -0,0 +1,16 @@
+tpl = ()
+
+bros = ("amal", "kamal", "nimal")
+sis = ("seetha", "geetha", "kamala")
+
+siblings = bros + sis  
+print(len(siblings))
+
+family_members = siblings + ("Amma", "Thaththa")
+print(family_members)
+
+animals_foods = ("dog", "cat", "fish", "bird", "turtle")
+fruits = ("apple", "banana", "orange", "grape", "mango")
+vegitables = ("carrot", "potato", "tomato", "cabbage", "onion")
+
+food_stuff_tp = fruits + vegitables + animals_foods
