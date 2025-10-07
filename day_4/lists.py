25bdccb9a5efef9eaf26215d3cba0e6cb25868b9
Lakshan Attanayake <lakshan.attanayake95@gmail.com>
2025-10-07 09:50:35 +0530

    lists day

--- day_4/lists.py
+++ day_4/lists.py
@@ -1,3 +1,25 @@
-fruits = ["Apple", "Banana", "Cherry"]
-first_fruit, second_fruit, third_fruit = fruits
-print(third_fruit)  # Output: Apple
+test = []
+foods = ['pizza', 'hamburger', 'hotdog', 'spaghetti']
+print(len(foods))
+
+print(foods[0])
+print(foods[2])
+print(foods[3])
+
+mixed_data_types = ['Kobe', 24, 3.14, True]
+print(mixed_data_types)
+
+mixed_data_types.append('test data')
+print(mixed_data_types)
+
+foods.extend(mixed_data_types)
+print(foods)
+
+print(mixed_data_types)
+
+mixed_data_types.append("python")
+mixed_data_types.append("SQL")
+mixed_data_types.append("Redux")
+
+full_stack = mixed_data_types
+print(full_stack)
