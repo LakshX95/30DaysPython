a3c859116a78bffa94232a974b54c35e96fe432e
Lakshan Attanayake <lakshan.attanayake95@gmail.com>
2025-10-10 12:03:27 +0530

    functions day

---
+++ day_11/functions.py
@@ -0,0 +1,77 @@
+# def generate_full_name():
+#     first_name = "Lakshan"
+#     last_name = "Attanayake"
+
+#     full_name = first_name + " " + last_name
+#     return full_name
+
+# print(generate_full_name())
+
+
+# def add_two_numbers():
+#     number1 = 3
+#     number2 = 5
+#     total = number1 + number2
+#     return total
+# print(add_two_numbers())
+
+# def greetings(name):
+#     massege = "Hello "+ name+ ", have a great day!!"
+#     return massege
+# print(greetings("Lakshan"))
+
+# def square_num(x):
+#     return x * x
+# print(square_num(100))
+
+# def area_of_circle(r):
+#     PI = 3.14
+#     area = PI * r ** 2
+#     return area
+# print(area_of_circle(3))
+
+# def sum_of_numbers(n):
+#     total = 0
+#     for i in range(n+1):
+#         total += i
+#     return total
+# print(sum_of_numbers(10))
+
+
+# def is_even(n):
+#     if n % 2 == 0:
+#         print("Even")
+#         return True
+#     return False
+# print(is_even(16))
+
+
+# def add_two_numbers(num1, num2):
+#     total = num1 + num2
+#     return total
+
+# print(add_two_numbers(4,7))
+
+# def area_of_circle(r):
+#     PI = 3.14
+#     area = PI * r ** 2
+#     return area
+# print(area_of_circle(2))
+
+# def add_all_nums(*args):
+#     total = 0
+#     for i in args:
+#         if not isinstance(i, (int, float)):
+#             return f"All arguments must be numbers. '{i}' is not a number."
+#         total += i
+#     return total
+# print(add_all_nums(2, 4, 6, 8))
+# print(add_all_nums(2, 4, 6, "9"))
+
+
+# °F = (°C x 9/5) + 32
+
+# def convert_celsius_to_fahrenheit(c):
+#     fahrenheit = (c * 9/5) + 32
+#     return fahrenheit
+# print(convert_celsius_to_fahrenheit(100))
