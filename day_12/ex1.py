532918e7838fa068cd58d4c9750dddd7218d130f
Lakshan Attanayake <lakshan.attanayake95@gmail.com>
2025-10-13 11:49:51 +0530

    added ex1 file

--- day_12/ex1.py
+++ day_12/ex1.py
@@ -7,3 +7,18 @@ def random_user_id():
     return user_id
 
 print(random_user_id())
+
+
+def user_id_gen_by_user():
+    num_chars = int(input("Enter number of characters per ID: "))
+    num_ids = int(input("Enter number of IDs to generate: "))
+
+    characters = string.ascii_lowercase + string.digits  # a-z + 0-9
+
+    for _ in range(num_ids):
+        user_id = ''.join(random.choice(characters) for _ in range(num_chars))
+        print(user_id)
+
+# Example usage
+user_id_gen_by_user()
+
